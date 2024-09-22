ee2250f1edcdb8294a5d7474d24d223077bf29a2
FahimReza <FahimReza202@outlook.com>
2024-09-23 00:55:34 +0330

    Save Debug Changes

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,8 +19,6 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.core import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('accounts/', include('accounts.urls')),
@@ -30,4 +28,4 @@ urlpatterns = [
 # Serving Static And Media For Development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
