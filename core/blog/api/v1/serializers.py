6b0358ac5e83b9a432817b9ddf2d66f6d825dd2e
FahimReza <FahimReza202@outlook.com>
2024-10-10 12:39:04 +0330

    Create The Crud With DRF

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
-class PostSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=200)
-    content = serializers.CharField(max_length=200)
+from ...models import Post
+
+
+
+# class PostSerializer(serializers.Serializer):
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=200)
+#     content = serializers.CharField(max_length=200)
+#     status = serializers.CharField(max_length=200)
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ["id" , "author" , "title" , "content" , "category" , "status" , "created_date" , "published_date"]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -5,13 +5,49 @@ from ...models import Post
 from .serializers import PostSerializer
 
 
-@api_view()
+@api_view(["GET","POST"])
 def post_list(request):
-    return Response({'name' : 'fahim'})
+    if request.method == "GET":
+        post = Post.objects.all()
+        serializer = PostSerializer(post , many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = PostSerializer(data=request.data)
+        """
+            Saving a post normally
+        """
 
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # else:
+        #     return Response(serializer.errors)
 
-@api_view()
+        """
+            Saving a post in a simpler way
+        """
+    
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+@api_view(["GET","PUT","DELETE"]) 
 def post_detail(request, id):
-    obj = Post.objects.get(pk=id)
-    serializer = PostSerializer(obj)
-    return Response(serializer.data)
+    # try :
+    #     obj = Post.objects.get(pk=id)
+    #     serializer = PostSerializer(obj)
+    #     return Response(serializer.data)
+    # except Post.DoesNotExist:
+    #     return Response('object does not exist' , status=404)
+    obj = get_object_or_404(Post,pk=id , status=True)
+    if request.method == "GET":
+        serializer = PostSerializer(obj)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = PostSerializer(obj,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        obj.delete()
+        return Response({'detail' : 'item removed successfully'})
