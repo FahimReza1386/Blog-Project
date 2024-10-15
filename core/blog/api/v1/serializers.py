7cc65e784b72e26b7239f0f065dd25bb2ce52b8e
FahimReza <FahimReza202@outlook.com>
2024-10-15 17:52:00 +0330

    Create The Chaneg Of Serializer

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -13,10 +13,26 @@ from ...models import Post , Category
 
 class PostSerializer(serializers.ModelSerializer):
 
+    """
+         Read Only Groups
+    """
+    # content = serializers.ReadOnlyField()
+    # content = serializers.CharField(read_only=True)
+
+    snipped = serializers.ReadOnlyField(source='get_snipped')
+    relative_url = serializers.URLField(source='get_absolute_api_url',read_only = True)
+    absolute_url = serializers.SerializerMethodField()
+
+
     class Meta:
         model = Post
-        fields = ["id" , "author" , "title" , "content" , "category" , "status" , "created_date" , "published_date"]
+        fields = ["id" , "author" , "title" , "content" ,'snipped','relative_url', 'absolute_url' ,  "category" , "status" , "created_date" , "published_date"]
+        # read_only_fields = ['title','content']
 
+
+    def get_absolute_url(self , obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri()
 
 class CategorySerializer(serializers.ModelSerializer):
 
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your models here.
 
 
@@ -26,6 +27,14 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+    
+
+    def get_snipped(self):
+        return self.content[0:5]
+    
+    def get_absolute_api_url(self):
+
+        return reverse('blog:api-v1:post-detail' , kwargs={'pk': self.pk})
     
 
 class Category(models.Model):
