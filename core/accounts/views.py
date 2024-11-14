0e40e5c2d34a6f5d71dfd960e0cea70958afa273
FahimReza <FahimReza202@outlook.com>
2024-11-14 13:50:00 +0330

    Use Django's default cache

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
+from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 from .tasks import sendEmail
 import time
@@ -12,8 +13,7 @@ def send_email(request):
     sendEmail.delay()
     return HttpResponse("<h1>Done Sending</h1>")
 
+@cache_page(60)
 def test(request):
-    if cache.get("test_delay_app") is None:
-        response = requests.get("https://914df9c0-90ab-43e3-b470-085908c96b27.mock.pstmn.io/test/delay/5")
-        cache.set("test_delay_app",response.json(),60)
-    return JsonResponse(cache.get('test_delay_app'))
+    response = requests.get("https://914df9c0-90ab-43e3-b470-085908c96b27.mock.pstmn.io/test/delay/5")
+    return JsonResponse(response.json())
