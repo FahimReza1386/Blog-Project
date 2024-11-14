3122b4ece5100a608cb86064a3012a65cbc854de
FahimReza <FahimReza202@outlook.com>
2024-11-14 13:15:15 +0330

    save the change of test url for cache.

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse , JsonResponse
+from django.core.cache import cache
 from .tasks import sendEmail
 import time
 import requests
@@ -12,5 +13,7 @@ def send_email(request):
     return HttpResponse("<h1>Done Sending</h1>")
 
 def test(request):
-    response = requests.get("https://914df9c0-90ab-43e3-b470-085908c96b27.mock.pstmn.io/test/delay/5")
-    return JsonResponse(response.json())
+    if cache.get("test_delay_app") is None:
+        response = requests.get("https://914df9c0-90ab-43e3-b470-085908c96b27.mock.pstmn.io/test/delay/5")
+        cache.set("test_delay_app",response.json(),60)
+    return JsonResponse(cache.get('test_delay_app'))
